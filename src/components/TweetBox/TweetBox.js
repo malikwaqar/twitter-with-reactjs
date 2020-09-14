ca203c82a817bae2014f087329ce41f8a8085ae1
Malik Waqar Ul Hassan <malikwaqarofficial@gmail.com>
2020-09-14 09:37:34 +0500

    TweetBox Completed

--- src/components/TweetBox/TweetBox.js
+++ src/components/TweetBox/TweetBox.js
@@ -10,7 +10,8 @@ function TweetBox() {
                     <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
                     <input placeholder="What's Happening?" type="text" />
                 </div>
-                <Button>Tweet</Button>
+                <input className="tweetBox__imageInput" placeholder="Enter image URL" type="text" />
+                <Button className="tweetBox__tweetButton">Tweet</Button>
             </form>
         </div>
     )
